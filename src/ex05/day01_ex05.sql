1ddfc652a73b36f04c1b107a4bfd81ab9ccbfdd7
elidacon <elidacon@student.21-school.ru>
2023-08-23 12:54:51 +0500

    Add fixes to ex 5, 8

--- src/ex05/day01_ex05.sql
+++ src/ex05/day01_ex05.sql
@@ -1,14 +1,7 @@
 -- write a SQL statement which returns all possible combinations between person
 -- and pizzeria tables and set ordering by person identifier and then by
 -- pizzeria identifier columns.
-SELECT person.id AS person_id,
-    person.name AS person_name,
-    age,
-    gender,
-    address,
-    pizzeria.id AS pizzeria_id,
-    pizzeria.name AS pizzeria_name,
-    rating
+SELECT *
 FROM person,
     pizzeria
 ORDER BY person.id,
--- src/ex08/day01_ex08.sql
+++ src/ex08/day01_ex08.sql
@@ -2,13 +2,13 @@
 -- The result must be the same like for exercise 07.
 -- It's now allowed to use: other types of JOINs
 SELECT order_date,
-    concat(name, ' (age:', age, ')') AS person_information
+    concat(person_mod.name, ' (age:', person_mod.age, ')') AS person_information
 FROM person_order
     NATURAL JOIN (
         SELECT id AS person_id,
             name,
             age
         FROM person
-    ) AS person
+    ) AS person_mod
 ORDER BY order_date,
     person_information;
