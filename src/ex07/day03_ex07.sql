cc2a49224476ec3cfe702f235a01385214c5a17c
elidacon <elidacon@student.21-school.ru>
2023-08-24 15:09:47 +0500

    Add day03 ex07 assignment

---
+++ src/ex07/day03_ex07.sql
@@ -0,0 +1,4 @@
+-- register a new pizza with name “greek pizza” (use id = 19) with price 800
+-- rubles in “Domino's” restaurant (pizzeria_id = 2)
+INSERT INTO menu (id, pizzeria_id, pizza_name, price)
+VALUES (19, 2, 'greek pizza', 800);
