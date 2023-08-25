7f27da1e97b6b50a04e099653e7737ac0e58a5ba
elidacon <elidacon@student.21-school.ru>
2023-08-25 12:59:12 +0500

    Team00: Add DDL for table creation (ex00)

---
+++ src/ex00/team00_ex00.sql
@@ -0,0 +1,18 @@
+CREATE TABLE nodes (
+    point1 VARCHAR NOT NULL,
+    point2 VARCHAR NOT NULL,
+    cost INT NOT NULL
+);
+INSERT INTO nodes
+VALUES ('a', 'b', 10),
+    ('b', 'a', 10),
+    ('a', 'c', 15),
+    ('c', 'a', 15),
+    ('a', 'd', 20),
+    ('d', 'a', 20),
+    ('b', 'c', 35),
+    ('c', 'b', 35),
+    ('b', 'd', 25),
+    ('d', 'b', 25),
+    ('c', 'd', 30),
+    ('d', 'c', 30);
