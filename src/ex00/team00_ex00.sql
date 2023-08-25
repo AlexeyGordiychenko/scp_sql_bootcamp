b47d2a4637272e0985ccb3d847de149e3d20cdfa
elidacon <elidacon@student.21-school.ru>
2023-08-25 22:48:32 +0500

    Team00: Add comments with the task and Cities.png

--- src/ex00/team00_ex00.sql
+++ src/ex00/team00_ex00.sql
@@ -1,3 +1,11 @@
+-- There are 4 cities (a, b, c and d) (see the picture Cities.png) and arcs
+-- between them with the cost ((a,b) = (b,a)). Create a table with name nodes by
+-- using structure {point1, point2, cost} and fill data based on a picture
+-- (remember there are direct and reverse paths between 2 nodes). Write one SQL
+-- statement that returns all tours (aka paths) with minimal traveling cost if
+-- we will start from city "a". Just remember, you need to find the cheapest way
+-- of visiting all the cities and returning to your starting point. For example,
+-- the tour looks like that a -> b -> c -> d -> a.
 CREATE TABLE nodes (
     point1 VARCHAR NOT NULL,
     point2 VARCHAR NOT NULL,
@@ -16,13 +24,10 @@ VALUES ('a', 'b', 10),
     ('d', 'b', 25),
     ('c', 'd', 30),
     ('d', 'c', 30);
-
-
-CREATE VIEW v_tsp AS
-WITH RECURSIVE toures AS (
+CREATE VIEW v_tsp AS WITH RECURSIVE toures AS (
     --first query is the starting point in the city 'a'
     SELECT point1 AS tour,
-        point1 AS start,
+        point1 AS START,
         point2 AS next,
         cost AS total_cost,
         1 AS length
@@ -40,11 +45,19 @@ WITH RECURSIVE toures AS (
         JOIN nodes AS n ON n.point1 = t.next
     WHERE t.tour NOT LIKE concat('%', n.point1, '%')
 )
-SELECT total_cost, concat('{', tour, ',', next, '}') AS tour
+SELECT total_cost,
+    concat('{', tour, ',', next, '}') AS tour
 FROM toures
-WHERE toures.start = toures.next and toures.length = (SELECT max(length) from toures);
-
+WHERE toures.start = toures.next
+    AND toures.length = (
+        SELECT max(length)
+        FROM toures
+    );
 SELECT *
 FROM v_tsp
-WHERE total_cost = (SELECT MIN(total_cost) from v_tsp)
-ORDER BY total_cost, tour;
+WHERE total_cost = (
+        SELECT MIN(total_cost)
+        FROM v_tsp
+    )
+ORDER BY total_cost,
+    tour;
--- src/ex01/team00_ex01.sql
+++ src/ex01/team00_ex01.sql
@@ -1,4 +1,14 @@
+-- add a possibility to see additional rows with the most expensive cost to the
+-- SQL from the previous exercise. Sort data by total_cost and then by tour.
 SELECT *
 FROM v_tsp
-WHERE total_cost = (SELECT MIN(total_cost) from v_tsp) OR total_cost = (SELECT MAX(total_cost) from v_tsp)
-ORDER BY total_cost, tour;
+WHERE total_cost = (
+        SELECT MIN(total_cost)
+        FROM v_tsp
+    )
+    OR total_cost = (
+        SELECT MAX(total_cost)
+        FROM v_tsp
+    )
+ORDER BY total_cost,
+    tour;
