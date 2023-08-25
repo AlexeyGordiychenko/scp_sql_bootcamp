08dd0f4e2560db5ae1e92cc28a143c25ffdb6208
elidacon <elidacon@student.21-school.ru>
2023-08-25 13:35:33 +0500

    Team00: Add recursive CTE query

--- src/ex00/team00_ex00.sql
+++ src/ex00/team00_ex00.sql
@@ -16,3 +16,36 @@ VALUES ('a', 'b', 10),
     ('d', 'b', 25),
     ('c', 'd', 30),
     ('d', 'c', 30);
+WITH RECURSIVE toures AS (
+    --first query is the starting point in the city 'a'
+    SELECT point1 AS tour,
+        point2 AS next,
+        cost AS total_cost,
+        1 AS length
+    FROM nodes
+    WHERE point1 = 'a'
+    UNION ALL
+    --second query is the recursive part to add all the possible unique
+    --combinations and sum the cost
+    SELECT concat(t.tour, ',', n.point1),
+        n.point2,
+        t.total_cost + n.cost,
+        t.length + 1
+    FROM toures AS t
+        JOIN nodes AS n ON n.point1 = t.next
+    WHERE t.tour NOT LIKE concat('%', n.point1, '%')
+),
+-- select only the full toures that go back to the starting point
+correct_toures AS (
+    SELECT tour,
+        next,
+        total_cost
+    FROM toures
+    WHERE next = 'a' -- return to the starting point
+        AND length = (
+            SELECT MAX(length)
+            FROM toures
+        )
+)
+SELECT *
+FROM correct_toures;
